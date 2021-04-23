2f5b4d9dee3ba2fc4bebf69eacc8bd7d3908767b
Akrise <49941855+Akrise@users.noreply.github.com>
2021-04-23 18:44:50 +0300

    Выполнено задание 2 урока 2

--- src/main/java/lesson2/Task_2.java
+++ src/main/java/lesson2/Task_2.java
@@ -1,4 +1,55 @@
 package lesson2;
 
 public class Task_2 {
+    public static void main(String[] args) {
+        Task_2 application = new Task_2();
+        int[][] chessBoard = new int[8][8];
+        application.fillChessArray(chessBoard);
+        Task_2.showWithFor(chessBoard);
+        Task_2.showWithForeach(chessBoard);
+        Task_2.showWithWhile(chessBoard);
+    }
+
+    private static void showWithWhile(int[][] chessBoard) {
+        int i = 0;
+        int j = 0;
+        System.out.println("ChessBoard printed with While:");
+        while (i < chessBoard.length) {
+            while (j < chessBoard[i].length) {
+                System.out.print(chessBoard[i][j++]);
+            }
+            System.out.println();
+            j = 0;
+            i++;
+        }
+    }
+
+    private static void showWithForeach(int[][] chessBoard) {
+        System.out.println("ChessBoard printed with foreach:");
+        for (int[] line : chessBoard) {
+            for (int square : line) {
+                System.out.print(square);
+            }
+            System.out.println();
+        }
+    }
+
+    private static void showWithFor(int[][] chessBoard) {
+        System.out.println("ChessBoard printed with for:");
+        for (int i = 0; i < chessBoard.length; i++) {
+            for (int j = 0; j < chessBoard[i].length; j++) {
+                System.out.print(chessBoard[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    private int[][] fillChessArray(int[][] chessBoard) {
+        for (int i = 0; i < chessBoard.length; i++) {
+            for (int j = 0; j < chessBoard[i].length; j++) {
+                chessBoard[i][j] = ((i + j) % 2 == 0 ? 0 : 1);
+            }
+        }
+        return chessBoard;
+    }
 }
