e54ec35e254359af7cd829afff90dcc1b367ca05
Akrise <49941855+Akrise@users.noreply.github.com>
2021-04-23 23:16:18 +0300

    Исправлены орфографические ошибки

--- src/main/java/lesson2/Task_6.java
+++ src/main/java/lesson2/Task_6.java
@@ -3,28 +3,28 @@ package lesson2;
 public class Task_6 {
     public static void main(String[] args) {
         Task_5 task4 = new Task_5();
-        int dimenshionM = 3 + (int) (Math.random() * 5);
-        int dimenshionN = 3 + (int) (Math.random() * 5);
-        int[][] inputMatrix = new int[dimenshionM][dimenshionN];
+        int dimensionM = 3 + (int) (Math.random() * 5);
+        int dimensionN = 3 + (int) (Math.random() * 5);
+        int[][] inputMatrix = new int[dimensionM][dimensionN];
         int[][] outputMatrix;
 
-        for (int i = 0; i < dimenshionM; i++) {
-            for (int j = 0; j < dimenshionN; j++) {
+        for (int i = 0; i < dimensionM; i++) {
+            for (int j = 0; j < dimensionN; j++) {
                 inputMatrix[i][j] = (int) (Math.random() * 2);
             }
         }
         outputMatrix = inputMatrix;
         //Определяем координаты двойки с учетом ограничения на края .
-        int invertorTwoMPosition = 1 + (int) (Math.random() * (dimenshionM - 2));
-        int invertorTwoNPosition = 1 + (int) (Math.random() * (dimenshionN - 2));
-        inputMatrix[invertorTwoMPosition][invertorTwoNPosition] = 2;
+        int inverterTwoMPosition = 1 + (int) (Math.random() * (dimensionM - 2));
+        int inverterTwoNPosition = 1 + (int) (Math.random() * (dimensionN - 2));
+        inputMatrix[inverterTwoMPosition][inverterTwoNPosition] = 2;
         System.out.println("Initial array:");
         Task_5.showMatrix(inputMatrix);
-        for (int i = invertorTwoNPosition + 1; i < dimenshionN; i++) {
-            outputMatrix[invertorTwoMPosition][i] = inputMatrix[invertorTwoMPosition][i] == 0 ? 1 : 0;
+        for (int i = inverterTwoNPosition + 1; i < dimensionN; i++) {
+            outputMatrix[inverterTwoMPosition][i] = inputMatrix[inverterTwoMPosition][i] == 0 ? 1 : 0;
         }
-        for (int i = invertorTwoMPosition + 1; i < dimenshionM; i++) {
-            for (int j = 0; j < dimenshionN; j++) {
+        for (int i = inverterTwoMPosition + 1; i < dimensionM; i++) {
+            for (int j = 0; j < dimensionN; j++) {
                 outputMatrix[i][j] = inputMatrix[i][j] == 0 ? 1 : 0;
             }
         }
