d26c07cdac9bf4be868383cabfc4d6416773cf78
Akrise <49941855+Akrise@users.noreply.github.com>
2021-04-23 23:46:58 +0300

    В задании 7 изменен тип возвращаемого значения с void на String в методе showAgeGroup

--- src/main/java/lesson2/Task_7.java
+++ src/main/java/lesson2/Task_7.java
@@ -4,27 +4,19 @@ public class Task_7 {
     public static void main(String[] args) {
         int[] testCases = {32, 52, 66, 82, 25, 44, 60, 75, 22, 119};
         for (int age : testCases) {
-            showAgeGroup(age);
+            System.out.println(showAgeGroup(age));
         }
     }
 
-    private static void showAgeGroup(int age) {
-        if (age >= 25 && age < 44) {
-            System.out.println("Молодой возраст");
-            return;
-        }
-        if (age >= 44 && age < 60) {
-            System.out.println("Средний возраст");
-            return;
-        }
-        if (age >= 60 && age < 75) {
-            System.out.println("Пожилой возраст");
-            return;
-        }
-        if (age >= 75 && age < 90) {
-            System.out.println("Старческий возраст");
-            return;
-        }
-        System.out.println("Неизвестный возраст");
+    private static String showAgeGroup(int age) {
+        if (age >= 25 && age < 44)
+            return "Молодой возраст";
+        if (age >= 44 && age < 60)
+            return "Средний возраст";
+        if (age >= 60 && age < 75)
+            return "Пожилой возраст";
+        if (age >= 75 && age < 90)
+            return "Старческий возраст";
+        return "Неизвестный возраст";
     }
 }
