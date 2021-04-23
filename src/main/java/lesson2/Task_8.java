1c6fc33b47833a1807a74d9356d63079ab3c18eb
Akrise <49941855+Akrise@users.noreply.github.com>
2021-04-23 23:41:30 +0300

    Выполнено задание 8 урока 2

---
+++ src/main/java/lesson2/Task_8.java
@@ -0,0 +1,45 @@
+package lesson2;
+
+public class Task_8 {
+    public static void main(String[] args) {
+        int[] testCases = {-1, 2, 4, 7, 0, 1, 3};
+        System.out.println("Switch with break:");
+        for (int inputNumber : testCases) {
+            switch (inputNumber) {
+                case 1:
+                    System.out.println("A");
+                    break;
+                case 2:
+                    System.out.println("AA");
+                    break;
+                case 3:
+                    System.out.println("AAA");
+                    break;
+                case 4:
+                    System.out.println("AAAA");
+                    break;
+                default:
+                    System.out.println("AAAAA");
+            }
+        }
+
+        System.out.printf("%nSwitch without break:%n");
+        for (int inputNumber : testCases) {
+            switch (inputNumber) {
+                default:
+                    System.out.print('A');
+                case 4:
+                    System.out.print('A');
+                case 3:
+                    System.out.print('A');
+                case 2:
+                    System.out.print('A');
+                case 1:
+                    System.out.println('A');
+
+            }
+        }
+
+    }
+}
+
