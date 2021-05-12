59ae145a17f03e5922b82bfd269b4cb6d62598a7
Akrise <49941855+Akrise@users.noreply.github.com>
2021-05-12 22:41:17 +0300

    Выполнено задание 1 урока 3

---
+++ src/main/java/homework_solution/lesson3/task1/Calculator.java
@@ -0,0 +1,19 @@
+package homework_solution.lesson3.task1;
+
+public class Calculator {
+    public int summ(int arg1, int arg2) {
+        return arg1 + arg2;
+    }
+
+    public int subtract(int minuend, int subtrahend) {
+        return minuend - subtrahend;
+    }
+
+    public int multiply(int arg1, int arg2) {
+        return arg1 * arg2;
+    }
+
+    public int divide(int divident, int divisor) {
+        return divident / divisor;
+    }
+}
