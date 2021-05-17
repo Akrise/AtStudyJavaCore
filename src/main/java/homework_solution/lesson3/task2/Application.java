4ac9281ffa0e3714eaebccd95aa237767bc4580e
Akrise <49941855+Akrise@users.noreply.github.com>
2021-05-18 00:02:54 +0300

    Выполнено задание 2 урока 3

---
+++ src/main/java/homework_solution/lesson3/task2/Application.java
@@ -0,0 +1,18 @@
+package homework_solution.lesson3.task2;
+
+import java.util.Arrays;
+
+public class Application {
+    public static void main(String[] args) {
+        Cat[] cats = { new Cat(2, 5, "Барсик"),
+                new Cat(3, 11, "Мурзик"),
+                new Cat(7, 2, "Матильда"),
+                new Cat(1, 3, "Леопольд"),
+                new Cat(11, 4, "Базилио") };
+        Arrays.sort(cats);
+        for (Cat cat: cats
+             ) {
+            cat.ShowData();
+        }
+    }
+}
---
+++ src/main/java/homework_solution/lesson3/task2/Cat.java
@@ -0,0 +1,21 @@
+package homework_solution.lesson3.task2;
+
+public class Cat implements Comparable<Cat> {
+    int age;
+    int weight;
+    String name;
+
+    public Cat(int age, int weight, String name) {
+        this.age = age;
+        this.weight = weight;
+        this.name = name;
+    }
+
+    void ShowData() {
+        System.out.printf("Имя: %s, Возраст: %d, Вес: %d%n", name, age, weight);
+    }
+
+    public int compareTo(Cat otherCat) {
+        return this.name.compareTo(otherCat.name);
+    }
+}
